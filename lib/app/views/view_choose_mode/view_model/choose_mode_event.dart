6b6bde133c441018b255ec8dad8f6985f9dea6aa
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-09-29 18:42:05 +0300

    add ✨ : choose mode page and state managment

---
+++ lib/app/views/view_choose_mode/view_model/choose_mode_event.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class ChooseModeEvent {}
+
+class ChooseModeInitialEvent extends ChooseModeEvent {
+  final ThemeData themeData;
+
+  ChooseModeInitialEvent(this.themeData);
+}
+
+class ChooseModeDarkEvent extends ChooseModeEvent {}
+
+class ChooseModeLightEvent extends ChooseModeEvent {}
