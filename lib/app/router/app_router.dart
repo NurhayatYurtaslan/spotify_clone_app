d23c6cdf8f24d4fb6c8d9b9a0e3b8dd5c2fbd97a
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-10-03 00:56:52 +0300

    add ✨ : router

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -1,6 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:spotify_clone_app/app/views/view_choose_mode/choose_mode_view.dart';
 import 'package:spotify_clone_app/app/views/view_intro/intro_view.dart';
+import 'package:spotify_clone_app/app/views/view_signin/signin_view.dart';
+import 'package:spotify_clone_app/app/views/view_signin_or_signup/signin_or_signup_view.dart';
+import 'package:spotify_clone_app/app/views/view_signup/signup_view.dart';
 import 'package:spotify_clone_app/app/views/view_splash/splash_view.dart';
 
 part "app_router.gr.dart";
@@ -14,5 +17,8 @@ class AppRouter extends RootStackRouter {
         AutoRoute(page: SplashViewRoute.page, initial: true),
         AutoRoute(page: IntroViewRoute.page),
         AutoRoute(page: ChooseModeViewRoute.page),
+        AutoRoute(page: SigninOrSignupViewRoute.page),
+        AutoRoute(page: SigninViewRoute.page),
+        AutoRoute(page: SignupViewRoute.page),
       ];
 }
