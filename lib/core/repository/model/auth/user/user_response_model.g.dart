29eba88436faf7fa9a431bbda1ee71f2f134c9e9
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-10-06 02:06:44 +0300

    add ✨: user response model

---
+++ lib/core/repository/model/auth/user/user_response_model.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_response_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$UserResponseModelImpl _$$UserResponseModelImplFromJson(
+        Map<String, dynamic> json) =>
+    _$UserResponseModelImpl(
+      fullName: json['fullName'] as String,
+      imageURL: json['imageURL'] as String,
+      email: json['email'] as String,
+    );
+
+Map<String, dynamic> _$$UserResponseModelImplToJson(
+        _$UserResponseModelImpl instance) =>
+    <String, dynamic>{
+      'fullName': instance.fullName,
+      'imageURL': instance.imageURL,
+      'email': instance.email,
+    };
