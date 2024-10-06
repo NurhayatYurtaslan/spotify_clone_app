cf6a387f6d700dd159527ff3d368fb98993eec30
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-10-06 15:49:04 +0300

    update 🆕 : ios service

--- lib/core/repository/service/auth_service.dart
+++ lib/core/repository/service/auth_service.dart
@@ -8,30 +8,29 @@ import 'package:spotify_clone_app/core/repository/model/auth/signin/signin_reque
 import 'package:spotify_clone_app/core/repository/model/auth/signup/signup_request_model.dart';
 import 'package:spotify_clone_app/core/repository/model/auth/user/user_response_model.dart';
 
-
-//* tüm auth islemleri, kayit ol, giris yap, cikis yap,  giris yaptigin bilgileri var mı kontrol et
+//tüm auth islemleri, kayit ol, giris yap, cikis yap,  giris yaptigin bilgileri var mı kontrol et
 class AuthService {
-  //* FirebaseAuth ve FirebaseFirestore örneklerinin oluşturulması.
+  // FirebaseAuth ve FirebaseFirestore örneklerinin oluşturulması.
   final FirebaseAuth _auth = FirebaseAuth.instance; //baglanıyoruz  auth
   final FirebaseFirestore _firestore =
       FirebaseFirestore.instance; //baglanıyoruz firestore
 
-  //* Kullanıcı kaydı işlemini gerçekleştiren fonksiyon.
+  // Kullanıcı kaydı işlemini gerçekleştiren fonksiyon.
   Future signUp(SignUpRequestModel signUpRequestModel) async {
-    //* Kullanıcının e-posta ve şifre ile Firebase'e kaydını sağlar.
+    // Kullanıcının e-posta ve şifre ile Firebase'e kaydını sağlar.
     final UserCredential
-        userCredential = //* kullanıcının kimlik bilgilerini ve Firebase’e giriş yaparken kullanılan yöntemi içerir.
+        userCredential = // kullanıcının kimlik bilgilerini ve Firebase’e giriş yaparken kullanılan yöntemi içerir.
         await _auth.createUserWithEmailAndPassword(
             email: signUpRequestModel.email,
             password: signUpRequestModel.password);
 
-    //* Kayıt işlemi sonrası dönen kullanıcı bilgileri.
+    // Kayıt işlemi sonrası dönen kullanıcı bilgileri.
     final User? user = userCredential.user;
     if (user == null) {
       throw Exception("User is null");
     }
 
-    //* Kullanıcının bilgilerini Firestore'daki "users" koleksiyonuna ekler.
+    // Kullanıcının bilgilerini Firestore'daki "users" koleksiyonuna ekler.
     await _firestore.collection("users").doc(user.uid).set(({
           "name":signUpRequestModel.name,
           "email":signUpRequestModel.email,
@@ -40,7 +39,7 @@ class AuthService {
     return signUpRequestModel;
   }
 
-//* Kullanıcının e-posta ve şifre ile Firebase'e giriş yapmasını sağlar.
+// Kullanıcının e-posta ve şifre ile Firebase'e giriş yapmasını sağlar.
   Future signIn(SignInRequestModel signInRequestModel) async {
     final UserCredential
         userCredential = //kullanıcının kimlik bilgilerini ve Firebase’e giriş yaparken kullanılan yöntemi içerir
@@ -48,7 +47,7 @@ class AuthService {
             email: signInRequestModel.email,
             password: signInRequestModel.password);
 
-    //* Giriş işlemi sonrası dönen kullanıcı bilgileri.
+    // Giriş işlemi sonrası dönen kullanıcı bilgileri.
     final User? user = userCredential.user;
     if (user == null) {
       throw Exception("User is null");
@@ -60,14 +59,14 @@ Future<User?> loginWithGoogle(BuildContext context) async {
     final googleUser = await GoogleSignIn().signIn();
 
     if (googleUser == null) {
-      //! Kullanıcı giriş yapma işlemini iptal etti.
+      // Kullanıcı giriş yapma işlemini iptal etti.
       throw Exception("Login with Goggle has been canceled");
     }
 
     final googleAuth = await googleUser.authentication;
 
     if (googleAuth.idToken == null && googleAuth.accessToken == null) {
-      //! Tokenlar alınamadı
+      // Tokenlar alınamadı
       throw Exception("Incorrect action taken");
     }
 
@@ -81,7 +80,7 @@ Future<User?> loginWithGoogle(BuildContext context) async {
 
     return userCredential.user;
   } catch (e) {
-    //* Hata durumunda kullanıcıya bilgi verin ve SignInPage'e geri dönün
+    // Hata durumunda kullanıcıya bilgi verin ve SignInPage'e geri dönün
     // ignore: use_build_context_synchronously
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
@@ -90,7 +89,7 @@ Future<User?> loginWithGoogle(BuildContext context) async {
       ),
     );
     
-    //* Geri dönme işlemi
+    // Geri dönme işlemi
     // ignore: use_build_context_synchronously
     context.router.push(const SigninViewRoute());
   }
@@ -98,34 +97,34 @@ Future<User?> loginWithGoogle(BuildContext context) async {
 }
 
 
-  //* Kullanıcının çıkış yapmasını sağlayan fonksiyon.
+  // Kullanıcının çıkış yapmasını sağlayan fonksiyon.
   Future signOut() async {
     await _auth.signOut();
   }
 
-//* Kullanıcının giriş yapıp yapmadığını kontrol eden fonksiyon.
+// Kullanıcının giriş yapıp yapmadığını kontrol eden fonksiyon.
   Future<bool> isSignedIn() async {
     final User? user = _auth.currentUser;
     return user != null;
   }
 
-  //* Mevcut kullanıcının bilgilerini döndüren fonksiyon.
+  // Mevcut kullanıcının bilgilerini döndüren fonksiyon.
   Future<User?> getUser() async {
     return _auth.currentUser;
   }
 
-  //* Mevcut kullanıcının detaylı bilgilerini Firestore'dan alıp döndüren fonksiyon.
+  // Mevcut kullanıcının detaylı bilgilerini Firestore'dan alıp döndüren fonksiyon.
   Future<UserResponseModel> getUserDetail() async {
     final User? user = _auth.currentUser;
     if (user == null) {
       throw Exception("User is null");
     }
 
-//* Firestore'dan kullanıcının detaylı bilgilerini alır.
+// Firestore'dan kullanıcının detaylı bilgilerini alır.
     DocumentSnapshot documentSnapshot =
         await _firestore.collection("users").doc(user.uid).get();
 
-//* Alınan bilgileri UserResponseModel nesnesine dönüştürür ve döndürür.
+// Alınan bilgileri UserResponseModel nesnesine dönüştürür ve döndürür.
     return UserResponseModel.fromDocumentSnapshot(documentSnapshot);
   }
 }
