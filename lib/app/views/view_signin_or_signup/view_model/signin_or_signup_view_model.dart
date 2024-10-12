c40a73dbc586c2f22f2832830ec7f229a5dc4e8f
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-10-13 01:25:45 +0300

    add ✨: modern snackbar

--- lib/app/views/view_signin_or_signup/view_model/signin_or_signup_view_model.dart
+++ lib/app/views/view_signin_or_signup/view_model/signin_or_signup_view_model.dart
@@ -1,5 +1,9 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modern_snackbar/modern_snackbar.dart';
+import 'package:spotify_clone_app/core/constants/color_constants.dart';
+import 'package:spotify_clone_app/core/extensions/context_extension.dart';
 import 'signin_or_signup_event.dart';
 import 'signin_or_signup_state.dart';
 import 'package:spotify_clone_app/app/router/app_router.dart'; // AutoRoute generated router
@@ -14,18 +18,47 @@ class SigninOrSignupViewModel
 
   Future<void> _onNavigateToSignin(
       NavigateToSigninEvent event, Emitter<SigninOrSignupState> emit) async {
-    event.context.router.push(const SigninViewRoute());
+        ModernSnackbar(
+        backgroundColor: AppColors.backColor,
+        description: 'Navigate to Signin Page.',
+        title: 'Please Wait',
+        icon: Icons.timer,
+        ).show(event.context);
+    Future.delayed(event.context.durationMedium, () {
+       
+          event.context.router.replace(const SigninViewRoute());
+       
+      });
   }
 
   Future<void> _onNavigateToSignup(
       NavigateToSignupEvent event, Emitter<SigninOrSignupState> emit) async {
     event.context.router.push(const SignupViewRoute());
+    ModernSnackbar(
+        backgroundColor: AppColors.backColor,
+        description: 'Navigate to Signup Page.',
+        title: 'Please Wait',
+        icon: Icons.timer,
+        ).show(event.context);
+    Future.delayed(event.context.durationMedium, () {
+       
+          event.context.router.replace(const SignupViewRoute());
+        
+      });
   }
 
-Future<void> _onBack(
-    BackEvent event, Emitter<SigninOrSignupState> emit) async {
-  event.context.router.replace(const ChooseModeViewRoute());
-  
-}
-
+  Future<void> _onBack(
+      BackEvent event, Emitter<SigninOrSignupState> emit) async {
+    ModernSnackbar(
+      backgroundColor: AppColors.backColor,
+        description: 'Returning to previous screen.',
+        title: 'Please Wait',
+        icon: Icons.timer,
+        ).show(event.context);
+    Future.delayed(event.context.durationMedium, () {
+        if (event.context.mounted) {
+          event.context.router.replace(const ChooseModeViewRoute());
+        }
+      });
+  }
 }
