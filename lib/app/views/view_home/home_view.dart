5f70c86aaa2320f9d767dc561e3bc084d8199fe9
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-10-19 23:44:55 +0300

    add ✨: widgets

--- lib/app/views/view_home/home_view.dart
+++ lib/app/views/view_home/home_view.dart
@@ -1,5 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify_clone_app/app/views/view_home/widgets/home_top_card_widget.dart';
+import 'package:spotify_clone_app/app/views/view_home/widgets/play_list_widget.dart';
+import 'package:spotify_clone_app/core/extensions/context_extension.dart';
+import 'package:spotify_clone_app/core/widgets/app_bar.dart';
+import 'package:spotify_clone_app/gen/assets.gen.dart';
 
 @RoutePage()
 class HomeView extends StatelessWidget {
@@ -7,6 +13,25 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: BasicAppbar(
+        hideBack: true,
+        onPressed: () {},
+        title: SvgPicture.asset(
+          Assets.images.svg.spotifyLogo,
+          height: context.mediumValue,
+          width: context.highValue,
+        ),
+      ),
+      body: const SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            HomeTopCardWidget(),
+            PlayListWidget(),
+          ],
+        ),
+      ),
+    );
   }
 }
