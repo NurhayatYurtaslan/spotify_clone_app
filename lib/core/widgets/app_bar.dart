589fd90f34d1e309f9e3d723dec4de68d9b49e64
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-10-20 18:28:54 +0300

    update 🆕 : app bar back button

--- lib/core/widgets/app_bar.dart
+++ lib/core/widgets/app_bar.dart
@@ -7,13 +7,15 @@ class BasicAppbar extends StatelessWidget implements PreferredSizeWidget {
   final Widget? action;
   final Color? backgroundColor;
   final bool hideBack;
-  const BasicAppbar(
-      {this.title,
-      this.hideBack = false,
-      this.action,
-      this.backgroundColor,
-      super.key,
-      required this.onPressed});
+
+  const BasicAppbar({
+    this.title,
+    this.hideBack = false,
+    this.action,
+    this.backgroundColor,
+    super.key,
+    required this.onPressed,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -26,17 +28,16 @@ class BasicAppbar extends StatelessWidget implements PreferredSizeWidget {
       leading: hideBack
           ? null
           : IconButton(
-              onPressed: () {
-                onPressed();
-              },
+              onPressed: onPressed,
               icon: Container(
                 height: 50,
                 width: 50,
                 decoration: BoxDecoration(
-                    color: context.isDarkMode
-                        ? Colors.white.withOpacity(0.03)
-                        : Colors.black.withOpacity(0.04),
-                    shape: BoxShape.circle),
+                  color: context.isDarkMode
+                      ? Colors.white.withOpacity(0.03)
+                      : Colors.black.withOpacity(0.04),
+                  shape: BoxShape.circle,
+                ),
                 child: Icon(
                   Icons.arrow_back_ios_new,
                   size: 15,
