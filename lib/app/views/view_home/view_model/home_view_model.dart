c299cb2015362e5c4fd77ae00392859cac28641e
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-10-21 23:34:35 +0300

    add ✨: see more view model

--- lib/app/views/view_home/view_model/home_view_model.dart
+++ lib/app/views/view_home/view_model/home_view_model.dart
@@ -1,41 +1,36 @@
-import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:cloud_firestore/cloud_firestore.dart'; // Firestore paketini içe aktar
 import 'package:spotify_clone_app/app/views/view_home/view_model/home_event.dart';
 import 'package:spotify_clone_app/app/views/view_home/view_model/home_state.dart';
 
 class HomeViewModel extends Bloc<HomeEvent, HomeState> {
-  HomeViewModel() : super(HomeInitialState(songs: [])) {
+  HomeViewModel() : super(HomeInitialState(songs: [], showSeeMore: true)) {
     on<HomeInitialEvent>(_initial);
+    on<SeeMoreEvent>(_seeMore);
   }
 
-  final FirebaseFirestore firestore = FirebaseFirestore.instance; // Firestore referansı
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  Future<FutureOr<void>> _initial(
-      HomeInitialEvent event, Emitter<HomeState> emit) async {
+  Future<void> _initial(HomeInitialEvent event, Emitter<HomeState> emit) async {
     try {
-      // Firestore'dan 'Songs' koleksiyonunu al
       QuerySnapshot snapshot = await firestore.collection('Songs').get();
 
       if (snapshot.docs.isNotEmpty) {
-        // Şarkıları Map formatında al ve listeye dönüştür
-        List<Map<String, dynamic>> songs = snapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
-        print("Fetched Songs: $songs");  // Veriyi kontrol için yazdır
-        songs.shuffle(); // Şarkıları karıştır
-        emit(HomeInitialState(songs: songs)); // Şarkıları emit et
+        List<Map<String, dynamic>> songs = snapshot.docs
+            .map((doc) => doc.data() as Map<String, dynamic>)
+            .toList();
+        print("Fetched Songs: $songs");
+        songs.shuffle();
+        emit(HomeInitialState(songs: songs, showSeeMore: true)); // Şarkıları emit et
       } else {
-        print("No songs found.");
-        emit(HomeInitialState(songs: [])); // Boş liste emit et
+        emit(HomeInitialState(songs: [], showSeeMore: false));
       }
-    } on FirebaseException catch (e) {
-      // Firestore'a özel hataları yakalayın
-      print("FirebaseException: ${e.message}"); // Hata mesajını yazdır
-      emit(HomeInitialState(songs: [])); // Hata durumunda boş liste emit et
-    } catch (e, stackTrace) {
-      // Diğer genel hataları yakalayın
-      print("General Error: ${e.toString()}");
-      print("Stack Trace: $stackTrace");  // Stack trace'i yazdır
-      emit(HomeInitialState(songs: [])); // Hata durumunda boş liste emit et
+    } catch (e) {
+      emit(HomeInitialState(songs: [], showSeeMore: false));
     }
+  }
+
+  void _seeMore(SeeMoreEvent event, Emitter<HomeState> emit) {
+    emit(state.copyWith(showSeeMore: false)); // See More'u gizle
   }
 }
