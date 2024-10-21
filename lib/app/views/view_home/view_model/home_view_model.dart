b81e6d5daafcb5a465f4afa1b9ddd528e898f815
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-10-22 00:06:07 +0300

    add ✨: see less view model

--- lib/app/views/view_home/view_model/home_view_model.dart
+++ lib/app/views/view_home/view_model/home_view_model.dart
@@ -4,9 +4,10 @@ import 'package:spotify_clone_app/app/views/view_home/view_model/home_event.dart
 import 'package:spotify_clone_app/app/views/view_home/view_model/home_state.dart';
 
 class HomeViewModel extends Bloc<HomeEvent, HomeState> {
-  HomeViewModel() : super(HomeInitialState(songs: [], showSeeMore: true)) {
+  HomeViewModel() : super(HomeInitialState(songs: [], showSeeMore: false)) {
     on<HomeInitialEvent>(_initial);
     on<SeeMoreEvent>(_seeMore);
+    on<SeeLessEvent>(_seeLess);
   }
 
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -19,18 +20,29 @@ class HomeViewModel extends Bloc<HomeEvent, HomeState> {
         List<Map<String, dynamic>> songs = snapshot.docs
             .map((doc) => doc.data() as Map<String, dynamic>)
             .toList();
-        print("Fetched Songs: $songs");
         songs.shuffle();
-        emit(HomeInitialState(songs: songs, showSeeMore: true)); // Şarkıları emit et
+        emit(HomeInitialState(songs: songs, showSeeMore: false)); // Başlangıçta gizli
       } else {
         emit(HomeInitialState(songs: [], showSeeMore: false));
       }
     } catch (e) {
+      // Hata günlüğü
+      print("Error fetching songs: $e");
       emit(HomeInitialState(songs: [], showSeeMore: false));
     }
   }
 
-  void _seeMore(SeeMoreEvent event, Emitter<HomeState> emit) {
-    emit(state.copyWith(showSeeMore: false)); // See More'u gizle
+ void _seeMore(SeeMoreEvent event, Emitter<HomeState> emit) {
+  print("See More Event Triggered");
+  if (!state.showSeeMore) {
+    emit(state.copyWith(showSeeMore: true)); // See More'u göster
   }
+}
+
+void _seeLess(SeeLessEvent event, Emitter<HomeState> emit) {
+  print("See Less Event Triggered");
+  if (state.showSeeMore) {
+    emit(state.copyWith(showSeeMore: false)); // Less More'u gizle
+  }
+}
 }
