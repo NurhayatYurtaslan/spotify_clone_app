6f6c0ab6cf8d215e64687c3ef47c6489d992aec1
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-10-21 23:34:21 +0300

    add ✨: see more state

--- lib/app/views/view_home/view_model/home_state.dart
+++ lib/app/views/view_home/view_model/home_state.dart
@@ -1,14 +1,27 @@
 abstract class HomeState {
-  List<dynamic>? songs;
-  List<dynamic>? playList;
-  HomeState({this.songs, this.playList});
+  final List<dynamic>? songs;
+  final List<dynamic>? playList;
+  final bool showSeeMore;  // Yeni eklenen alan
+
+  HomeState({this.songs, this.playList, required this.showSeeMore});
+
+  HomeState copyWith({
+    List<dynamic>? songs,
+    List<dynamic>? playList,
+    bool? showSeeMore,
+  }) {
+    return HomeInitialState(
+      songs: songs ?? this.songs,
+      playList: playList ?? this.playList,
+      showSeeMore: showSeeMore ?? this.showSeeMore,
+    );
+  }
 }
 
 class HomeInitialState extends HomeState {
-  List<dynamic>? songs;
-  List<dynamic>? playList;
-  HomeInitialState({this.songs, this.playList})
-      : super(songs: songs, playList: playList);
+  HomeInitialState({super.songs, super.playList, required super.showSeeMore});
 }
 
-//add favorite state
+class SeeMoreState extends HomeState {
+  SeeMoreState({super.songs, super.playList, required super.showSeeMore});
+}
