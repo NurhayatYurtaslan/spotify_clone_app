dd83509139c55579da59296ec38a1cc69d1e37da
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-10-21 23:35:14 +0300

    add ✨: for see more functions

--- lib/app/views/view_home/widgets/play_list_widget.dart
+++ lib/app/views/view_home/widgets/play_list_widget.dart
@@ -1,30 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:spotify_clone_app/core/constants/color_constants.dart';
 import 'package:spotify_clone_app/core/extensions/context_extension.dart';
+import 'package:spotify_clone_app/core/helpers/is_dark_mode.dart';
 
 class PlayListWidget extends StatelessWidget {
   const PlayListWidget({
     super.key,
+    required this.showSeeMore,
+    required this.onSeeMorePressed, // Callback fonksiyonu ekledik
   });
+
+  final bool showSeeMore; // See More durumunu tutmak için bir değişken
+  final VoidCallback
+      onSeeMorePressed; // See More butonuna tıklanınca çalışacak fonksiyon
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(
           vertical: context.mediumValue, horizontal: context.mediumValue),
-      child: const Column(
+      child: Column(
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(
+              const Text(
                 'Playlist',
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
               ),
-              Text(
-                'See More',
-                style: TextStyle(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 12,
+              GestureDetector(
+                onTap:
+                    onSeeMorePressed, // Butona tıklandığında çalışacak fonksiyon
+                child: Text(
+                  'See More',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12,
+                    color: context.isDarkMode
+                        ? AppColors.grey
+                        : AppColors.darkGrey,
+                  ),
                 ),
               ),
             ],
