04524f012a62de39c9fe845926c76c5bb2ee3a6c
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-10-29 22:20:43 +0300

    add ✨: drawer widget

---
+++ lib/core/widgets/drawer_widget.dart
@@ -0,0 +1,48 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:spotify_clone_app/app/router/app_router.dart';
+import 'package:spotify_clone_app/core/extensions/context_extension.dart';
+import 'package:spotify_clone_app/gen/assets.gen.dart';
+
+class DrawerWidget extends StatelessWidget {
+  const DrawerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            child: Center(
+              child: Image.asset(
+                Assets.images.png.drawerImage.path,
+                width: context.width * .2,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text("Home Page"),
+            onTap: () {
+              context.router.replace(HomeViewRoute());
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const Text("Profile"),
+            onTap: () {
+              context.router.replace(const ProfileViewRoute());
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text("Settings"),
+            onTap: () {
+              context.router.replace(const SettingsViewRoute());
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
