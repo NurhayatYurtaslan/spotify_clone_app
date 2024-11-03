79eed294bd3d7989e8c379ac4a54210279c406f2
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-11-03 19:50:51 +0300

    add ✨: favorite hive box

--- lib/starter.dart
+++ lib/starter.dart
@@ -5,31 +5,40 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:spotify_clone_app/app/app.dart';
+import 'package:spotify_clone_app/core/repository/model/songs/songs_model.dart';
 import 'package:spotify_clone_app/firebase_options.dart';
 
 const String logLevelKey = "5";
+
 launchApp() async {
   if (kDebugMode) {
-    if (kDebugMode) {
-      print('LogLevel set for this flavor:$logLevelKey');
-    }
+    print('LogLevel set for this flavor: $logLevelKey');
   }
 
   if (Flavor.I.isDevelopment) {
-    if (kDebugMode) {
-      debugPrint('Development mode');
-    }
+    debugPrint('Development mode');
   }
 
-  //Hive
+  // Hive
   await Hive.initFlutter();
-  await Hive.openBox('favorite');
+  
+  // Adapter kaydetme
+  Hive.registerAdapter(SongAdapter()); 
+  
+  // Kutu açmadan önce kontrol et
+  if (!Hive.isBoxOpen('favorite')) {
+    await Hive.openBox<Song>('favorite'); // Kutu açık değilse aç
+  }
+
+  // Diğer kutular
   await Hive.openBox('settings');
   await Hive.openBox('userLocalDB');
-  //Firebase
+  
+  // Firebase
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  //Remove StatusBar
+  
+  // StatusBar'ı kaldır
   SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
 
